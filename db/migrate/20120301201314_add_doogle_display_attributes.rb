0f4a02ddcffc894e0d2268e8f53e67dc9a9d25d5
Tim Harrison <heedspin@gmail.com>
2012-03-03 13:16:39 -0500

    Be sure to put doogle in filename so it can be ignored.

--- db/migrate/20120301201314_add_doogle_display_attributes.rb
+++ db/migrate/20120301201314_add_doogle_display_attributes.rb
@@ -1,7 +1,7 @@
-class AddDisplayAttributes < ActiveRecord::Migration
+class AddDoogleDisplayAttributes < ActiveRecord::Migration
   def up
-    # add_column :displays, :bonding_type_id, :integer
-    # add_column :displays, :backlight_color_id, :integer
+    add_column :displays, :bonding_type_id, :integer
+    add_column :displays, :backlight_color_id, :integer
     add_column :displays, :graphic_type_id, :integer
     add_column :displays, :character_rows, :integer
     add_column :displays, :character_columns, :integer
