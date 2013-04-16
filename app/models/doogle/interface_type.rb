0dc04a67051dccd759346536247137a86cf6e956
Tim Harrison <heedspin@gmail.com>
2013-04-16 10:29:56 -0400

    Add eDP interface.

--- app/models/doogle/interface_type.rb
+++ app/models/doogle/interface_type.rb
@@ -23,6 +23,7 @@ class Doogle::InterfaceType < ApplicationModel
     {:id => 9,  :name => 'PC'},
     {:id => 10, :name => 'MIPI'},
     {:id => 11, :name => 'MPU'},
-    {:id => 12, :name => 'Serial'}
+    {:id => 12, :name => 'Serial'},
+    {:id => 13, :name => 'eDP'}
   ]
 end
--- lib/doogle/version.rb
+++ lib/doogle/version.rb
@@ -1,3 +1,3 @@
 module Doogle
-  VERSION = '6.0.1'
+  VERSION = '6.1.0'
 end
