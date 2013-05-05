78bf9fcbc25b9174ec6a5b9d6736c4ee566365ec
Tim Harrison <heedspin@gmail.com>
2013-05-05 12:57:51 -0400

    Open gaping hole in order to test this...

--- app/controllers/display_assets_controller.rb
+++ app/controllers/display_assets_controller.rb
@@ -2,39 +2,54 @@ require 'menu_selected'
 
 class DisplayAssetsController < ApplicationController
   include MenuSelected
+  skip_before_filter :require_login
+
+  before_filter :maybe_require_login, :only => :show
+  def maybe_require_login
+    if request.env['HTTP_USER_AGENT'].include?('Excel')
+      true
+    else
+      true #require_login
+    end
+  end
+  
   def show
     logger.info request.env.select {|k,v| k.match("^HTTP.*")}.inspect
     logger.info request.env.select {|k,v| k.match(".*requested.*")}.inspect
-    @display = current_object
-    asset = params[:asset] || :datasheet
-    version = params[:version]
-    # Basic security check; make sure it's at least a field.
-    if Doogle::FieldConfig.for_key(asset).nil?
-      not_found
+    if request.env['HTTP_USER_AGENT'].include?('Excel')
+      render :text => 'hello excel', :status => 200
     else
-      spec = if version
-        @display.spec_versions.version(version).first
-      else
-        @display.spec_versions.latest.first
-      end
-      if spec.nil?
+      @display = current_object
+      asset = params[:asset] || :datasheet
+      version = params[:version]
+      # Basic security check; make sure it's at least a field.
+      if Doogle::FieldConfig.for_key(asset).nil?
         not_found
       else
-        asset_is_public = spec.asset_public?(asset)
-        if asset_is_public or permitted_to?("read_#{asset}", :doogle_displays)
-          if attachment = spec.send(asset)
-            redirect_to attachment.expiring_url(300), :status => 307
-          else
-            not_found
-          end
+        spec = if version
+          @display.spec_versions.version(version).first
         else
-          not_authorized
+          @display.spec_versions.latest.first
+        end
+        if spec.nil?
+          not_found
+        else
+          asset_is_public = spec.asset_public?(asset)
+          if asset_is_public or permitted_to?("read_#{asset}", :doogle_displays)
+            if attachment = spec.send(asset)
+              redirect_to attachment.expiring_url(300), :status => 307
+            else
+              not_found
+            end
+          else
+            not_authorized
+          end
         end
       end
     end
   end
 
-  skip_before_filter :require_login, :only => [:options]
+  # skip_before_filter :require_login, :only => [:options]
   def options
     logger.info request.env.select {|k,v| k.match("^HTTP.*")}.inspect
     logger.info request.env.select {|k,v| k.match(".*requested.*")}.inspect
