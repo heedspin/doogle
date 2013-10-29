7ee0d22b2d5be3eab058813cba1d61d9aefc7073
Tim Harrison <heedspin@gmail.com>
2013-10-29 13:45:32 -0400

    Add customer name.  Ignore missing cost / qty.

--- app/models/doogle/incomplete_display_list.rb
+++ app/models/doogle/incomplete_display_list.rb
@@ -12,6 +12,7 @@ class Doogle::IncompleteDisplayList
   end
 
   def xls_initialize
+    xls_field('Customer') { |d| d.original_opportunity.try(:sales_customer_name) }
     xls_field('Notes') { |d| d.original_opportunity.try(:safe_title) }
     xls_field('Created Date') { |d| d.created_at }
     xls_field('Model Number') { |d| d.model_number }
@@ -23,7 +24,7 @@ class Doogle::IncompleteDisplayList
     (1..3).each do |index|
 	    xls_field("Pricing #{index}") { |d| 
 	    	if p = d.prices.active.order(:id)[index-1]
-	    		(p.cost1.present? and p.notes.present?) ? 'X' : '' 
+	    		p.notes.present? ? 'X' : '' 
 	    	else
 	    		'n/a'
 	    	end
@@ -47,7 +48,7 @@ class Doogle::IncompleteDisplayList
   	Doogle::Display.not_deleted.created_after(start_date).without_displays(@displays.values).includes(:prices).select('displays.id').group('displays.id').having('count(display_prices.id) = 0').each do |d|
   		@displays[d.id] ||= Doogle::Display.find(d.id)
   	end
-  	Doogle::DisplayPrice.not_deleted.active.created_after(start_date).without_displays(@displays.values).includes(:display).where('(display_prices.cost1 is null or display_prices.cost1 = \'\') or (display_prices.notes is null or display_prices.notes = \'\')').each do |p|
+  	Doogle::DisplayPrice.not_deleted.active.created_after(start_date).without_displays(@displays.values).includes(:display).where('display_prices.notes is null or display_prices.notes = \'\'').each do |p|
   		@displays[p.id] ||= p.display
   	end
   	Doogle::Display.not_deleted.created_after(start_date).without_displays(@displays.values).includes(:spec_versions).select('displays.id').group('displays.id').having('count(doogle_spec_versions.id) = 0').each do |d|
