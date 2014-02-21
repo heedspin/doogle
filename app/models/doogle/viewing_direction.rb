7f3263572bceac530982d64f1bdcd14a1de2ee4f
Tim Harrison <heedspin@gmail.com>
2014-02-21 09:43:34 -0500

    Add full view direction. Add publish to web to master list.

--- app/models/doogle/viewing_direction.rb
+++ app/models/doogle/viewing_direction.rb
@@ -1,20 +1,16 @@
 require 'plutolib/active_hash_methods'
 class Doogle::ViewingDirection < ActiveHash::Base
   self.data = [
-    {:position => 1, :id => 1, :cmethod => 'twelve', :name => '12:00'},
-    {:position => 2, :id => 5, :cmethod => 'one_thirty', :name => '1:30'},
-    {:position => 3, :id => 2, :cmethod => 'three', :name => '3:00'},
-    {:position => 4, :id => 6, :cmethod => 'four_thirty', :name => '4:30'},
-    {:position => 5, :id => 3, :cmethod => 'six', :name => '6:00'},
-    {:position => 6, :id => 7, :cmethod => 'seven_thirty', :name => '7:30'},
-    {:position => 7, :id => 4, :cmethod => 'nine', :name => '9:00'},
-    {:position => 8, :id => 8, :cmethod => 'ten_thirty', :name => '10:30'},
+    {:id => 1, :cmethod => 'twelve', :name => '12:00'},
+    {:id => 5, :cmethod => 'one_thirty', :name => '1:30'},
+    {:id => 2, :cmethod => 'three', :name => '3:00'},
+    {:id => 6, :cmethod => 'four_thirty', :name => '4:30'},
+    {:id => 3, :cmethod => 'six', :name => '6:00'},
+    {:id => 7, :cmethod => 'seven_thirty', :name => '7:30'},
+    {:id => 4, :cmethod => 'nine', :name => '9:00'},
+    {:id => 8, :cmethod => 'ten_thirty', :name => '10:30'},
+    {:id => 9, :cmethod => 'full', :name => 'Full'},
   ]
   include Plutolib::ActiveHashMethods
-  
-  def self.all
-    super.sort_by(&:position)
-  end
-  
   
 end
--- lib/doogle/version.rb
+++ lib/doogle/version.rb
@@ -1,3 +1,3 @@
 module Doogle
-  VERSION = '9.3.0'
+  VERSION = '9.4.0'
 end
