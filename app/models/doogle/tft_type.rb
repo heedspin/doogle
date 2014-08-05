93e456bc8783db1d940f1141386d37fa76afe419
Tim Harrison <heedspin@gmail.com>
2014-08-05 12:07:41 -0400

    Add F-MVA.

--- app/models/doogle/tft_type.rb
+++ app/models/doogle/tft_type.rb
@@ -3,7 +3,8 @@ class Doogle::TftType < ActiveHash::Base
   self.data = [
     {:id => 1, :name => 'TN'},
     {:id => 2, :name => 'IPS'},
-    {:id => 3, :name => 'MVA'}
+    {:id => 3, :name => 'MVA'},
+    {:id => 4, :name => 'F-MVA'}
   ]
   include Plutolib::ActiveHashMethods
 end
--- lib/doogle/version.rb
+++ lib/doogle/version.rb
@@ -1,3 +1,3 @@
 module Doogle
-  VERSION = '9.8.0'
+  VERSION = '9.9.0'
 end
