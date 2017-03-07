bb5bc4eb6fef137216cc03acf0057f95d18e64bc
Gabe Berke-Williams <gabebw@gabebw.com>
2017-03-07 15:22:29 -0800

    Copy in some dependencies from old LXD code
    Doogle has a few required Ruby classes that it needs. Until now, it
    didn't need to provide them because the Rails apps that use Doogle
    provided them. The lxdinc-new code doesn't provide them, so now Doogle
    ships with the classes:
    
    * `AppConfig`
    * `ApplicationModel`
    * `MenuSelected`

---
+++ lib/menu_selected.rb
@@ -0,0 +1,30 @@
+# Copied from old LXD codebase
+module MenuSelected
+  def self.included(base)
+    base.class_eval <<-EOS
+    helper_method :menu_selected
+    EOS
+  end
+
+  SELECTED_CSS_CLASS='current'
+
+  def menu_selected(args={})
+    result = if path_regex = args[:path]
+      path_regex.match(request.path).present?
+    elsif path_starts_with = args[:path_starts_with]
+      request.path.starts_with?(path_starts_with)
+    else
+      parent_key = args[:parent]
+      controller_names = args[:controllers] || args[:controller] || self.controller_name
+      controller_names = if controller_names.is_a?(Enumerable)
+        controller_names.map(&:to_s)
+      else
+        [ controller_names.to_s ]
+      end
+      action = (args[:action] || self.action_name).to_s
+      (parent_key.nil? || params[parent_key.to_s.singularize + '_id'].present?) && (controller_names.include?(self.controller_name)) && (action == self.action_name)
+    end
+    result ? SELECTED_CSS_CLASS : ''
+  end
+
+end
