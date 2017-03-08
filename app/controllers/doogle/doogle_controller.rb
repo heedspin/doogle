01f1b291a564fd49a7631b64536813600be1e9d5
Gabe Berke-Williams <gabebw@gabebw.com>
2017-03-08 12:51:41 -0800

    Monkeypatch for Rails 5
    This adds `hide_action` and `filter_access_filter` to
    ActionController::Base.

--- app/controllers/doogle/doogle_controller.rb
+++ app/controllers/doogle/doogle_controller.rb
@@ -1,4 +1,25 @@
 require 'menu_selected'
+
+# declarative_authorization calls hide_action, which doesn't exist
+# in Rails 5. So, monkeypatch it for Rails 5. It also immediately inserts itself
+# into ActionController::Base when it's require'd, so we must monkeypatch
+# ActionController::Base before require-ing it.
+class ActionController::Base
+  before_filter :filter_access_filter
+
+  def self.hide_action(*names)
+    if Rails::VERSION::MAJOR >= 5
+      send(:private, *names)
+    else
+      super
+    end
+  end
+
+  # Define it so that declarative_authorization can skip it
+  def self.filter_access_filter
+  end
+end
+
 require 'declarative_authorization'
 
 class Doogle::DoogleController < ApplicationController
